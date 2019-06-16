810e0caa2a33b09f614e8940d0a3f844ba2f6b00
George G <george.g@list.ru>
2019-06-16 13:55:06 +0300

    jquery

---
+++ 06-16-01-jquery/main.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(){
+//      $(".block").css('background-color', 'blue');
+$(".block").toggleClass("red");
+
+$('.block').click(function () {
+    let $this = $(this);
+    $this.toggleClass("green");
+});
+
+$('body').on('click', '#block', function () {
+    alert('рфшёл');
+});
+
+$("#block").on('click',  function () {
+    alert('рфшёл2');
+});
+
+$('body').append('<div id="block"> block </div>');
+
+$(".block2").text('<div>hello</div>');
+
+$(".block3").wrap('<div class="d3"><span></span></div>');
+
+$(".block").unbind('click');
+
+console.log($("img.pretty").attr("src"));
+
+$("img.pretty").attr("src", "details1.png");
+// });
+
+// let jq = jQuery.noConflict();
+// $(document).ready(function(){
+//
+// };
